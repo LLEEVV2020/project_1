58f24eecfb68b60de05a5ea4dc0f49145f555b77
LLEEVV2020 <lleevv2020@gmail.com>
2022-10-27 16:07:42 +0400

    add

--- gulpfile.js
+++ gulpfile.js
@@ -34,10 +34,10 @@ function styles() {
  function scripts (){
   return src([
     'node_modules/jquery/dist/jquery.js',
-    'node_modules/slick-carousel/slick/slick.js',
     'app/js/jquery.maskedinput.js',
     'app/js/jquery.fancybox.min.js',
     'app/js/feedback.js',
+    'node_modules/slick-carousel/slick/slick.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
