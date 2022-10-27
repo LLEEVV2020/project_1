c897504d61c7ff4bb2f8e8eb053be8a8f2c1fd50
LLEEVV2020 <lleevv2020@gmail.com>
2022-10-27 14:56:18 +0400

    add

--- app/js/main.js
+++ app/js/main.js
@@ -48,6 +48,34 @@ $(document).ready(function () {
     $('html, body').animate({           
     scrollTop:  $(anchor).offset().top  
     }, 600);                            
+  });
+
+
+  
+
+  $(document).ready(function() {
+    $('.modal__form').submit(function() { // проверка на пустоту заполненных полей. Атрибут html5 — required не подходит (не поддерживается Safari)
+      
+      if (this.name.value == '' || this.phone.value == '' || this.phone.email == '' ) {
+        valid = false;
+        return valid;
+      }
+      $.ajax({
+        type: "POST",
+        url: "mail.php",
+        data: $(this).serialize()
+      }).done(function() {
+        //$('.js-overlay-thank-you').fadeIn();
+        $(this).find('input').val('');
+        $('.modal__form').trigger('reset');
+      });
+      return false;
     });
+  });
+
+  // Маска ввода номера телефона (плагин maskedinput)
+  $(function($){
+    $('[name="phone"]').mask("+7(999) 999-9999");
+  });
 
 })
